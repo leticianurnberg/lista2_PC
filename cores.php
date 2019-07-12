210e77f308334ad834b00944653eca37a736529d
leticianurnberg <48133429+leticianurnberg@users.noreply.github.com>
2019-07-12 19:14:46 -0300

    Create cores.php

---
+++ cores.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>atividade prática 2 - seletor de cores Interativo</title>
+</head>
+<body>
+    <form action="2sel_cores.php" method="post">
+        <h1>Seletor de Cores Interativo</h1>
+        <br>
+        <label>R:</label><input type="text" name="red"> <br> <br>
+        <label>G:</label><input type="text" name="green"> <br> <br>
+        <label>B:</label><input type="text" name="blue"> <br> <br>
+        <button type="submit">Mostrar cor</button>
+    </form>
+</body>
+</html>
